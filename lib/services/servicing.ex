e6c8321344ec8de8daa9b9b3ec3a26dc3866ce18
Phuntsho <pwangmo2019@gmail.com>
2025-10-29 14:23:32 +0600

    feat: Adds categories

---
+++ lib/services/servicing.ex
@@ -0,0 +1,147 @@
+defmodule Services.Servicing do
+  @moduledoc """
+  The Servicing context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Services.Repo
+
+  alias Services.Servicing.Category
+  alias Services.Accounts.Scope
+
+  @doc """
+  Subscribes to scoped notifications about any category changes.
+
+  The broadcasted messages match the pattern:
+
+    * {:created, %Category{}}
+    * {:updated, %Category{}}
+    * {:deleted, %Category{}}
+
+  """
+  def subscribe_categories(%Scope{} = scope) do
+    key = scope.user.id
+
+    Phoenix.PubSub.subscribe(Services.PubSub, "user:#{key}:categories")
+  end
+
+  defp broadcast_category(%Scope{} = scope, message) do
+    key = scope.user.id
+
+    Phoenix.PubSub.broadcast(Services.PubSub, "user:#{key}:categories", message)
+  end
+
+  @doc """
+  Returns the list of categories.
+
+  ## Examples
+
+      iex> list_categories(scope)
+      [%Category{}, ...]
+
+  """
+  def list_categories(%Scope{} = scope) do
+    Repo.all_by(Category, user_id: scope.user.id)
+  end
+
+  @doc """
+  Gets a single category.
+
+  Raises `Ecto.NoResultsError` if the Category does not exist.
+
+  ## Examples
+
+      iex> get_category!(scope, 123)
+      %Category{}
+
+      iex> get_category!(scope, 456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_category!(%Scope{} = scope, id) do
+    Repo.get_by!(Category, id: id, user_id: scope.user.id)
+  end
+
+  @doc """
+  Creates a category.
+
+  ## Examples
+
+      iex> create_category(scope, %{field: value})
+      {:ok, %Category{}}
+
+      iex> create_category(scope, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_category(%Scope{} = scope, attrs) do
+    with {:ok, category = %Category{}} <-
+           %Category{}
+           |> Category.changeset(attrs, scope)
+           |> Repo.insert() do
+      broadcast_category(scope, {:created, category})
+      {:ok, category}
+    end
+  end
+
+  @doc """
+  Updates a category.
+
+  ## Examples
+
+      iex> update_category(scope, category, %{field: new_value})
+      {:ok, %Category{}}
+
+      iex> update_category(scope, category, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def update_category(%Scope{} = scope, %Category{} = category, attrs) do
+    true = category.user_id == scope.user.id
+
+    with {:ok, category = %Category{}} <-
+           category
+           |> Category.changeset(attrs, scope)
+           |> Repo.update() do
+      broadcast_category(scope, {:updated, category})
+      {:ok, category}
+    end
+  end
+
+  @doc """
+  Deletes a category.
+
+  ## Examples
+
+      iex> delete_category(scope, category)
+      {:ok, %Category{}}
+
+      iex> delete_category(scope, category)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_category(%Scope{} = scope, %Category{} = category) do
+    true = category.user_id == scope.user.id
+
+    with {:ok, category = %Category{}} <-
+           Repo.delete(category) do
+      broadcast_category(scope, {:deleted, category})
+      {:ok, category}
+    end
+  end
+
+  @doc """
+  Returns an `%Ecto.Changeset{}` for tracking category changes.
+
+  ## Examples
+
+      iex> change_category(scope, category)
+      %Ecto.Changeset{data: %Category{}}
+
+  """
+  def change_category(%Scope{} = scope, %Category{} = category, attrs \\ %{}) do
+    true = category.user_id == scope.user.id
+
+    Category.changeset(category, attrs, scope)
+  end
+end
---
+++ lib/services_web/live/category_live/index.ex
@@ -0,0 +1,74 @@
+defmodule ServicesWeb.CategoryLive.Index do
+  use ServicesWeb, :live_view
+
+  alias Services.Servicing
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
+      <.header>
+        Listing Categories
+        <:actions>
+          <.button variant="primary" navigate={~p"/categories/new"}>
+            <.icon name="hero-plus" /> New Category
+          </.button>
+        </:actions>
+      </.header>
+
+      <.table
+        id="categories"
+        rows={@streams.categories}
+        row_click={fn {_id, category} -> JS.navigate(~p"/categories/#{category}") end}
+      >
+        <:col :let={{_id, category}} label="Name">{category.name}</:col>
+        <:col :let={{_id, category}} label="Description">{category.description}</:col>
+        <:action :let={{_id, category}}>
+          <div class="sr-only">
+            <.link navigate={~p"/categories/#{category}"}>Show</.link>
+          </div>
+          <.link navigate={~p"/categories/#{category}/edit"}>Edit</.link>
+        </:action>
+        <:action :let={{id, category}}>
+          <.link
+            phx-click={JS.push("delete", value: %{id: category.id}) |> hide("##{id}")}
+            data-confirm="Are you sure?"
+          >
+            Delete
+          </.link>
+        </:action>
+      </.table>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    if connected?(socket) do
+      Servicing.subscribe_categories(socket.assigns.current_scope)
+    end
+
+    {:ok,
+     socket
+     |> assign(:page_title, "Listing Categories")
+     |> stream(:categories, list_categories(socket.assigns.current_scope))}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    category = Servicing.get_category!(socket.assigns.current_scope, id)
+    {:ok, _} = Servicing.delete_category(socket.assigns.current_scope, category)
+
+    {:noreply, stream_delete(socket, :categories, category)}
+  end
+
+  @impl true
+  def handle_info({type, %Services.Servicing.Category{}}, socket)
+      when type in [:created, :updated, :deleted] do
+    {:noreply, stream(socket, :categories, list_categories(socket.assigns.current_scope), reset: true)}
+  end
+
+  defp list_categories(current_scope) do
+    Servicing.list_categories(current_scope)
+  end
+end
