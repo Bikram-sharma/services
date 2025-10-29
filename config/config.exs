aaf22a74432b7f7710c8e02f9016a8931ce9b765
Bikram Sharma <sharmabikram061@gmail.com>
2025-10-29 10:36:11 +0600

    feat: Adds auth system

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :services, :scopes,
+  user: [
+    default: true,
+    module: Services.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Services.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :services,
   ecto_repos: [Services.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
