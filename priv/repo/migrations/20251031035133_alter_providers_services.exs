715c515cfc2896d27161e85f6abff6a33f3971b3
Bikram Sharma <sharmabikram061@gmail.com>
2025-10-31 10:10:21 +0600

    fix:removes user_id form providers_service table

---
+++ priv/repo/migrations/20251031035133_alter_providers_services.exs
@@ -0,0 +1,9 @@
+defmodule Services.Repo.Migrations.AlterProvidersServices do
+  use Ecto.Migration
+
+  def change do
+    alter table(:providers_service) do
+    remove :user_id
+    end
+  end
+end
