95bae3078674ac7081f71d2a9936dd16332409cb
dawa.t <dawadorji295@gmail.com>
2025-11-04 15:28:11 +0600

    feat: Adds universal plug module

--- lib/services_web/controllers/page_controller.ex
+++ lib/services_web/controllers/page_controller.ex
@@ -2,9 +2,6 @@ defmodule ServicesWeb.PageController do
   use ServicesWeb, :controller
   alias Services.Servicing
 
-
-
-
   def home(conn, _params) do
     is_hidden = Servicing.is_hidden(conn.assigns.current_scope)
     render(conn, :home, is_hidden: is_hidden)
