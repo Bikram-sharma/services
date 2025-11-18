50afb5c7e5c82f5e0d6ced15ef42bc84d1f82d1d
Phuntsho <pwangmo2019@gmail.com>
2025-11-18 16:21:21 +0600

    feats:Adds booking form

--- lib/services_web/controllers/dash_controller.ex
+++ lib/services_web/controllers/dash_controller.ex
@@ -7,7 +7,7 @@ defmodule ServicesWeb.DashController do
 
     is_hidden = Servicing.is_hidden(conn.assigns.current_scope)
 
-     categories = Servicing.list_categories()
+    categories = Servicing.list_categories()
     providers_service = case params["category_id"] do
       nil ->
         ProviderService.list_providers_service()
@@ -20,7 +20,17 @@ defmodule ServicesWeb.DashController do
     |> render(:dash, is_hidden: is_hidden, categories: categories)
   end
 
-  def book(conn, %{"id" => id}) do
-    render(conn, :book, id: id)
+  def book(conn, %{"id" => id} = params) do
+    categories = Servicing.list_categories()
+    providers_service = case params["category_id"] do
+      nil ->
+        ProviderService.list_providers_service()
+      category_id ->
+        ProviderService.list_providers_service_by_category(category_id)
+    end
+    conn
+    |> assign(:providers_service, providers_service)
+    |> assign(:selected_category, params["category_id"] || "")
+    |> render(:book, id: id, categories: categories)
   end
 end
