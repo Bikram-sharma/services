cac469d2753edecfe08ef97cd4d252c7bafc8a1c
dawa.t <dawadorji295@gmail.com>
2025-11-27 16:45:50 +0600

    fix(bookings-table): alter bookings table
    adds column; :initiated_at,:booking_status, :contact,:payment_status, :delivered_at, :accepted_at

--- lib/services/bookings/booking.ex
+++ lib/services/bookings/booking.ex
@@ -11,6 +11,12 @@ defmodule Services.Bookings.Booking do
     field :cancelled_by, :string
     field :actual_total_price, :decimal
     field :user_address, :string
+    field :initiated_at, :utc_datetime
+    field :booking_status, :string
+    field :contact, :string
+    field :payment_status, :string
+    field :delivered_at, :utc_datetime
+    field :accepted_at, :utc_datetime
 
     belongs_to :users, Services.Accounts.User,
       foreign_key: :user_id,
