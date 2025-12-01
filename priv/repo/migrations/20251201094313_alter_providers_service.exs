b3e072820affc138874c9235bc53b364113dd047
dawa.t <dawadorji295@gmail.com>
2025-12-01 16:30:35 +0600

    fix: delete duplicate alter_providers_service.exs

--- priv/repo/migrations/20251201094313_alter_providers_service.exs
+++
@@ -1,9 +0,0 @@
-defmodule Services.Repo.Migrations.AlterProvidersService do
-  use Ecto.Migration
-
-  def change do
-    alter table(:providers_service) do
-      add :descriptions, :text
-    end
-  end
-end
