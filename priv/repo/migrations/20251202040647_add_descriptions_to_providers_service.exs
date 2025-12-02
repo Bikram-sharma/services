850b8ede7fa5f3c617ba73f478a4ad97e867cfc2
dawa.t <dawadorji295@gmail.com>
2025-12-02 10:18:36 +0600

    fix: Debug
    Adds migration table for providers_service and also adds style

---
+++ priv/repo/migrations/20251202040647_add_descriptions_to_providers_service.exs
@@ -0,0 +1,9 @@
+defmodule Services.Repo.Migrations.AlterProvidersService do
+  use Ecto.Migration
+
+  def change do
+    alter  table(:providers_service) do
+      add :descriptions, :text
+    end
+  end
+end
