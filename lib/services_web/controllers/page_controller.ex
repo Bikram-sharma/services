e42eff6ebcd215fbcc4544683902c305266e1d30
dawa.t <dawadorji295@gmail.com>
2025-12-05 16:48:26 +0600

    fix: redesign home page

--- lib/services_web/controllers/page_controller.ex
+++ lib/services_web/controllers/page_controller.ex
@@ -1,9 +1,13 @@
 defmodule ServicesWeb.PageController do
   use ServicesWeb, :controller
+  alias Services.Servicing
+  alias Services.ProviderService
 
   def home(conn, _params) do
     conn
     |> assign(:ratings, Services.Ratings.list_ratings())
+    |> assign(:providers_service, ProviderService.list_providers_service())
+    |> assign(:categories, Servicing.list_categories())
     |> render()
   end
 end
